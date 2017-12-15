da50042b7b5f519c0529e011a63e8197c37b54ed
tesya <tesyasakun@gmail.com>
2017-12-15 11:33:47 +0700

    first commit

---
+++ app/src/main/java/com/lulua/tesyant/rental/SignUpActivity.java
@@ -0,0 +1,85 @@
+package com.lulua.tesyant.rental;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "";
+    private EditText edtEmail, edtPassword;
+    private Button btnSignup;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        btnSignup = findViewById(R.id.btn_signup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = edtEmail.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), R.string.enter_email, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), R.string.enter_password, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 8) {
+                    Toast.makeText(getApplicationContext(), R.string.password_short, Toast.LENGTH_SHORT).show();
+                }
+
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "is complete " + task.isSuccessful());
+                                if (!task.isSuccessful()) {
+                                    try {
+                                        throw task.getException();
+                                    }
+                                    catch (FirebaseAuthUserCollisionException emailExists) {
+                                        Toast.makeText(SignUpActivity.this, "Email has been already registered", Toast.LENGTH_SHORT).show();
+                                    }
+                                    catch (Exception e) {
+                                        Log.d(TAG, "On Complete " + e.getMessage());
+                                    }
+                                }
+
+                                else {
+                                    Toast.makeText(getApplicationContext(), R.string.successful, Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+}
